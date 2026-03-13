c58897df9f56f221b6bffa09dad569543dac7353
wellnesschallenge2026-beep <wellnesschallenge2026@gmail.com>
2026-03-13 21:31:32 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 //  Wellness Challenge 2026 — Configuration
 // ============================================================
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyX7ZRhaz8X46FjerlJdDtR1SRpgSwIAZSzFWE19epp5g67vMyt5KIz3i6dF-t27MZo_Q/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby4casVzbLhm6S6FHpZgjONSj6Hta5Sl6FPWTvGeAj-tPBIk5JUxudvRFb5jprKjeYTcA/exec",
   CHALLENGE_START: "2026-01-01",
   CHALLENGE_END: "2026-12-31",
 };
